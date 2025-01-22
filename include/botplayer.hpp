edcc5c657bb98f6a9210b4d02c61574b9e14321d
dani-neto <daniellesantananeto2@gmail.com>
2025-01-22 13:12:26 -0300

    Create botplayer.hpp
    estava dando um erro de prioridade sobre um membro da classe base. c

---
+++ include/botplayer.hpp
@@ -0,0 +1,50 @@
+#ifndef BOTPLAYER_HPP
+#define BOTPLAYER_HPP
+
+#include <vector>
+#include <utility>
+#include "jogos.hpp" 
+
+// Classe base abstrata para representar bots em jogos de tabuleiro
+class BotPlayer {
+public:
+    virtual ~BotPlayer() = default;
+
+    // Método abstrato para calcular a próxima jogada do bot
+    virtual std::pair<int, int> calcularProximaJogada(
+        const JogosDeTabuleiro& jogo, 
+        int jogadorAtual
+    ) = 0;
+};
+
+// Classe para implementar o bot do jogo Reversi
+class ReversiBot : public BotPlayer {
+public:
+    // Sobrescreve o método da classe base
+    std::pair<int, int> calcularProximaJogada(
+        const JogosDeTabuleiro& jogo, 
+        int jogadorAtual
+    ) override;
+};
+
+// Classe para implementar o bot do jogo Lig 4
+class Lig4Bot : public BotPlayer {
+public:
+    // Sobrescreve o método da classe base
+    std::pair<int, int> calcularProximaJogada(
+        const JogosDeTabuleiro& jogo, 
+        int jogadorAtual
+    ) override;
+};
+
+// Classe para implementar o bot do jogo da velha
+class JogoDaVelhaBot : public BotPlayer {
+public:
+    // Sobrescreve o método da classe base
+    std::pair<int, int> calcularProximaJogada(
+        const JogosDeTabuleiro& jogo, 
+        int jogadorAtual
+    ) override;
+};
+
+#endif // BOTPLAYER_HPP
