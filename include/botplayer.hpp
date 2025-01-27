44782bcf2d771851394e9ea82738839aed0a2987
Joao Lisboa <90469235+joaovclisboa@users.noreply.github.com>
2025-01-27 19:17:09 -0300

    Update botplayer.hpp

--- include/botplayer.hpp
+++ include/botplayer.hpp
@@ -2,6 +2,7 @@
 #define BOTPLAYER_HPP
 #include <vector>
 #include <utility>
+#include "jogos.hpp"
 
 // Classe base abstrata para representar bots em jogos de tabuleiro
 class BotPlayer {
@@ -13,8 +14,8 @@ public:
         const JogosDeTabuleiro& jogo, 
         int jogadorAtual
     ) = 0;
+
 protected:
-    // Proibindo cópia e atribuição acidental
     BotPlayer() = default;
     BotPlayer(const BotPlayer&) = delete;
     BotPlayer& operator=(const BotPlayer&) = delete;
@@ -23,31 +24,39 @@ protected:
 // Classe para implementar o bot do jogo Reversi
 class ReversiBot : public BotPlayer {
 public:
-    // Sobrescreve o método da classe base
     std::pair<int, int> calcularProximaJogada(
         const JogosDeTabuleiro& jogo, 
         int jogadorAtual
     ) override;
+
+private:
+    int _minimax(Reversi& jogo, int profundidade, bool maximizando, int jogadorAtual);
+    int _avaliarTabuleiro(const Reversi& jogo, int jogadorAtual);
 };
 
 // Classe para implementar o bot do jogo Lig 4
 class Lig4Bot : public BotPlayer {
 public:
-    // Sobrescreve o método da classe base
     std::pair<int, int> calcularProximaJogada(
-        const JogosDeTabuleiro& jogo, 
+        const JogosDeTabuleiro& jogoBase, 
         int jogadorAtual
     ) override;
+
+private:
+    int _minimax(Lig4& jogo, bool maximizando, int jogadorAtual);
 };
 
 // Classe para implementar o bot do jogo da velha
 class JogoDaVelhaBot : public BotPlayer {
 public:
-    // Sobrescreve o método da classe base
     std::pair<int, int> calcularProximaJogada(
-        const JogosDeTabuleiro& jogo, 
+        const JogosDeTabuleiro& jogoBase, 
         int jogadorAtual
     ) override;
+
+private:
+    void minimax(const JogoDaVelha& jogo, int& melhorLinha, int& melhorColuna, int jogador);
+    int _minimax(JogoDaVelha& jogo, int profundidade, bool maximizando, int jogador);
 };
 
-#endif 
+#endif
